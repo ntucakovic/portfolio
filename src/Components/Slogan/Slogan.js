763367f0ac4238205b80d1a4aa9b0ba2fc1191e0
Nikola Tucakovic <nikola@byteout.com>
2017-10-01 02:47:11 +0200

    Use setState callback for doing synchronous changes. Pass active state from child to parent in order to hide siblings.

--- src/Components/Slogan/Slogan.js
+++ src/Components/Slogan/Slogan.js
@@ -3,6 +3,33 @@ import PropTypes from 'prop-types';
 import SloganIconLink from '../SloganIconLink/SloganIconLink';
 
 class Slogan extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      hasActiveLinkClassName: ''
+    };
+
+    this.hasActiveLinkClassName = 'is-active';
+
+    this.stateChangeDelay = 0;
+    this.stateChangeTimeout = null;
+
+    this.handleStateChange = this.handleStateChange.bind(this);
+  }
+
+  handleStateChange (e) {
+    if (this.stateChangeTimeout) {
+      clearTimeout(this.stateChangeTimeout);
+    }
+
+    this.stateChangeTimeout = setTimeout(() => {
+      this.setState({
+        hasActiveLinkClassName: (e.state.isActive ? this.hasActiveLinkClassName : '')
+      });
+    }, this.stateChangeDelay);
+  }
+
   render () {
     return (
       <div>
@@ -14,24 +41,24 @@ class Slogan extends Component {
             for <span className='text-emphasis'>Web Design </span>
             and <span className='text-emphasis'>UI</span>/<span className='text-emphasis'>UX</span></p>
 
-          <ul className='slogan__icons'>
+          <ul className={`slogan__icons ${this.state.hasActiveLinkClassName}`}>
             <li>
-              <SloganIconLink iconName='envelope' href='mailto:nt@ntmedia.me' title={this.props.iconTitles.email} />
+              <SloganIconLink iconName='envelope' href='mailto:nt@ntmedia.me' title={this.props.iconTitles.email} onStateChange={this.handleStateChange} />
             </li>
             <li>
-              <SloganIconLink iconName='linkedin' href='https://rs.linkedin.com/in/nikolatucakovic' target='_blank' rel='noopener noreferrer' title={this.props.iconTitles.linkedin} />
+              <SloganIconLink iconName='linkedin' href='https://rs.linkedin.com/in/nikolatucakovic' target='_blank' rel='noopener noreferrer' title={this.props.iconTitles.linkedin} onStateChange={this.handleStateChange} />
             </li>
             <li>
-              <SloganIconLink iconName='twitter' href='https://twitter.com/_ntucakovic' target='_blank' rel='noopener noreferrer' title={this.props.iconTitles.twitter} />
+              <SloganIconLink iconName='twitter' href='https://twitter.com/_ntucakovic' target='_blank' rel='noopener noreferrer' title={this.props.iconTitles.twitter} onStateChange={this.handleStateChange} />
             </li>
             <li>
-              <SloganIconLink iconName='instagram' href='https://www.instagram.com/nikola.tucakovic/' target='_blank' rel='noopener noreferrer' title={this.props.iconTitles.instagram} />
+              <SloganIconLink iconName='instagram' href='https://www.instagram.com/nikola.tucakovic/' target='_blank' rel='noopener noreferrer' title={this.props.iconTitles.instagram} onStateChange={this.handleStateChange} />
             </li>
             <li>
-              <SloganIconLink iconName='skype' href='skype:ntmediasolutions?chat' title={this.props.iconTitles.skype} />
+              <SloganIconLink iconName='skype' href='skype:ntmediasolutions?chat' title={this.props.iconTitles.skype} onStateChange={this.handleStateChange} />
             </li>
             <li>
-              <SloganIconLink iconName='file' href='http://www.ntmedia.me/documents/cv.pdf' title={this.props.iconTitles.cv} />
+              <SloganIconLink iconName='file' href='http://www.ntmedia.me/documents/cv.pdf' title={this.props.iconTitles.cv} onStateChange={this.handleStateChange} />
             </li>
           </ul>
         </header>
--- src/Components/SloganIconLink/SloganIconLink.js
+++ src/Components/SloganIconLink/SloganIconLink.js
@@ -36,15 +36,20 @@ class SloganIconLink extends Component {
   }
 
   handleEnter () {
-    this.setState({ isTransitioning: true });
-    this.enterTimeout = setTimeout(() => {
-      this.setState({
-        isActive: true,
-        isTransitioning: false,
-        linkActiveStateClassName: this.linkActiveClassName
-      });
-      clearTimeout(this.enterTimeout);
-    }, this.timeoutDelay);
+    this.setState({ isTransitioning: true }, () => {
+      this.enterTimeout = setTimeout(() => {
+        this.setState({
+          isActive: true,
+          isTransitioning: false,
+          linkActiveStateClassName: this.linkActiveClassName
+        }, () => {
+          clearTimeout(this.enterTimeout);
+
+          // Call parent method to update class on Slogan container.
+          this.props.onStateChange(this);
+        });
+      }, this.timeoutDelay);
+    });
   }
 
   handleLeave () {
@@ -56,13 +61,17 @@ class SloganIconLink extends Component {
       isTransitioning: true,
       isActive: false,
       linkActiveStateClassName: ''
+    }, () => {
+      this.props.onStateChange(this);
     });
   }
 
   render () {
-    const { iconName, ...htmlAttributes } = this.props;
+    const { iconName, onStateChange, ...htmlAttributes } = this.props;
     return (
-      <a className='slogan__link' {...htmlAttributes} onClick={this.handleClick} onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>
+      <a className={`slogan__link ${this.state.linkActiveStateClassName}`} {...htmlAttributes}
+        onClick={this.handleClick} onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>
+
         <SloganIcon className={`slogan__icon ${this.state.linkActiveStateClassName}`} iconName={iconName} />
         <span className={`slogan__title ${this.state.linkActiveStateClassName}`}>{this.props.title}</span>
       </a>
@@ -72,7 +81,8 @@ class SloganIconLink extends Component {
 
 SloganIconLink.propTypes = {
   iconName: PropTypes.string,
-  title: PropTypes.string
+  title: PropTypes.string,
+  onStateChange: PropTypes.func
 };
 
 export default SloganIconLink;
