dffb158945faf28df20423bf3f5ae14d6a76b5a2
Nikola Tucaković <nt@ntmedia.me>
2019-03-04 22:13:18 +0100

    Implement dark mode style

---
+++ src/modules/matchMedia.js
@@ -0,0 +1,23 @@
+function matchMedia (mediaQuery) {
+  try {
+    const mediaQueryList = window.matchMedia(mediaQuery);
+
+    if (mediaQueryList.media !== mediaQuery) {
+      // Browser doesn't support tested media query.
+      return null;
+    }
+
+    return mediaQueryList.matches;
+  } catch {
+    // Browser doesn't support matchMedia.
+    return null;
+  }
+}
+
+function isDarkMode () {
+  const mediaQuery = "(prefers-color-scheme: dark)";
+  return matchMedia(mediaQuery);
+}
+
+export { isDarkMode };
+export default matchMedia;
