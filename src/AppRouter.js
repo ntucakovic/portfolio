277775f39abf93638a012b26c33ecb6074fe3a4f
Nikola Tucaković <nt@ntmedia.me>
2019-03-04 20:58:59 +0100

    Always redirect to Home path until I implement rest of the pages

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 
 const AppRouter = () => (
   <main>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/index.html" component={Home} />
+      <Route render={() => (
+        <Redirect to="/" />
+      )} />
     </Switch>
   </main>
 );
